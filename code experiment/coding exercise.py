2e80a8a536ed990473b628f61cbac78786b7d335
Ashwani Jangra <ashwanijangra72@gmail.com>
2023-03-21 23:22:54 +0530

    add edit button

--- code experiment/coding exercise.py
+++ code experiment/coding exercise.py
@@ -3,20 +3,38 @@ import PySimpleGUI as sg
 
 label1 = sg.Text("type a text:")
 input1 = sg.Input(tooltip='enter text', key='text')
-button1 = sg.Button('add')
+button1 = sg.Button('Add')
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=function.python_project(),key= 'texts',enable_events=True,
+                      size=[55,12])
+edit_button = sg.Button("Edit")
 
-window = sg.Window("my text app", layout= [[label1,input1,button1]],font=15)
+window = sg.Window("My Text App", layout=[[label1, input1, button1],
+                   [list_box, edit_button]], font=(20))
 
 while True:
-    item , value =  window.read()
-    print(item)
-    print(value)
-    match item:
-        case'add':
+    event, values = window.read()
+    print(1, event)
+    print(2, values)
+    print(3, values['texts'])
+    match event:
+        case'Add':
             todo = function.python_project()
-            new_todo = value['text'] + '\n'
+            new_todo = values['text'] + '\n'
             todo.append(new_todo)
             function.write_todos(todo)
+            window['texts'].update(values['text'])
+        case'Edit':
+            todo_to_edit = values['texts'][0]
+            new_todo = values['text']
+
+            todo = function.python_project()
+            index = todo.index(todo_to_edit)
+            todo[index] = new_todo
+            function.write_todos(todo)
+            window['texts'].update(values= todo)
+        case'texts':
+            window['text'].update(value=values['texts'][0])
         case sg.WIN_CLOSED:
             break
 
--- code experiment/function.py
+++ code experiment/function.py
@@ -1,13 +1,14 @@
 def python_project(filepath= "D:\python file\members.txt"):
     """read the file and return the list of present data """
-    with open(filepath,'r')as file:
-        text= file.readlines()
+    with open(filepath, 'r')as file:
+        text = file.readlines()
     return text
 
 
-def write_todos(text,filepath="D:\python file\members.txt"):
-    with open(filepath,"w") as file:
+def write_todos(text, filepath= "D:\python file\members.txt"):
+    with open(filepath, "w") as file:
         file.writelines(text)
+
 
 if __name__ == "__name__":
     print("hello")
