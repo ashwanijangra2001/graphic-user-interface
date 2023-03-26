22d82fb1ac9b644a1c0f27ec79828e159241a886
Ashwani Jangra <ashwanijangra72@gmail.com>
2023-03-26 20:17:20 +0530

    add the image as a button image .

--- code experiment/coding exercise.py
+++ code experiment/coding exercise.py
@@ -6,12 +6,15 @@ sg.theme("black")
 clock = sg.Text(" ", key='clock')
 label1 = sg.Text("type a text:")
 input1 = sg.Input(tooltip='enter text', key='text')
-button1 = sg.Button('Add', font=10)
+button1 = sg.Button(size=4, image_source=r"D:\python file\add.png",
+                    mouseover_colors="lightBlue",
+                    tooltip='add item', key='Add')
 edit_button = sg.Button("Edit")
 list_box = sg.Listbox(values=function.python_project(),key= 'texts',enable_events=True,
                       size=[55,12])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(image_source="D:\python file\complete.png",
+                            mouseover_colors="lightBlue",key="Complete")
 exit_button = sg.Button('Exit')
 
 window = sg.Window("My Text App", layout=[[clock], [label1, input1, button1],
@@ -23,14 +26,11 @@ while True:
     window['clock'].update(value= time.strftime("%b %d ,%Y   %H:%M:%S"))
     match event:
         case'Add':
-            try:
-                todo = function.python_project()
-                new_todo = values['text'] + '\n'
-                todo.append(new_todo)
-                function.write_todos(todo)
-                window['texts'].update(values=todo)
-            except TypeError:
-                sg.popup("please select the item first.", font=('classic',20))
+            todo = function.python_project()
+            new_todo = values['text'] + '\n'
+            todo.append(new_todo)
+            function.write_todos(todo)
+            window['texts'].update(values=todo)
         case'Edit':
             try:
                 todo_to_edit = values['texts'][0]
