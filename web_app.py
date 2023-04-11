89822f590eb4dbbf2c9618dabed89663fbaf579f
Ashwani Jangra <ashwanijangra72@gmail.com>
2023-04-11 21:58:46 +0530

    add complete feature in web app

--- web_app.py
+++ web_app.py
@@ -3,17 +3,24 @@ import function
 
 todos = function.python_project()
 
-st.title("MY TODO APP")
-st.subheader("This is my todo app")
-st.write("This app is increase the productivity:")
+def add_todo():
+    todo = st.session_state['new_todo'] +'\n'
+    todos.append(todo)
+    function.write_todos(todos)
 
-for todo in todos:
-    st.checkbox(todo)
+st.title("To do app")
+st.subheader("This is my todo list.")
+st.write("This app is to increase your productivity.")
 
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.text_input(label='', placeholder='add a new todo...')
-
-
-
+st.text_input(label="", placeholder='Add new todo....',
+              on_change=add_todo, key='new_todo')
 
 
